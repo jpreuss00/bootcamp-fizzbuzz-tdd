71d41577a2dcc0eed1f7601665f0976daf2b10e1
Preuß <joshua.preuss2000@gmx.de>
2019-11-25 14:20:06 +0100

    implementTest

--- src/main/java/org/example/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/fizzbuzz/FizzBuzz.java
@@ -3,11 +3,27 @@ package org.example.fizzbuzz;
 public class FizzBuzz {
 
     public static void main(String...args) {
-    	// TODO implement me
+        for (int i = 1; i < 101; i++){
+        String fizzBuzz = fizzbuzz(i);
+        if (i < 100){
+        System.out.print(fizzBuzz + ", ");
+        } else {
+        System.out.print(fizzBuzz);
+        System.out.println(" ");
+            }
+        }
     }
 
     public static String fizzbuzz(int number) {
-    	// TODO implement me
-	    return Integer.toString(15);
+        if (number % 15 == 0){
+            return "FizzBuzz";
+        }
+        else if (number % 3 == 0){
+            return "Fizz";
+        }
+        else if (number % 5 == 0){
+            return "Buzz";
+        }
+        return Integer.toString(number);
+        }
     }
-}
